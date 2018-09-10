ce0975577cae7e3d067d0c3cbcf3abc5ad3dd971
iancarr1412 <iancarr1412@live.co.uk>
2018-09-10 11:46:20 +0100

    20180910 - test

--- djangobin/settings.py
+++ djangobin/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-as!g6p0%*ex4!sfif3%uh1@ryz_elkwe6!(!)su#b&@&fyrlj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [localhost]
 
 
 # Application definition
